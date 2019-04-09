e843b63ad371d207f421384c9225678db4dceb4f
JDVapor <jdvapordestiny@gmail.com>
2019-04-08 21:15:13 -0700

    working

--- liri.js
+++ liri.js
@@ -1,18 +1,14 @@
 require("dotenv").config();
-
+var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var axios = require("axios");
 var moment = require('moment');
 var keys = require("./keys.js");
-
-// var spotify = new Spotify(keys.spotify);
-
+var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 
 const concertSearch = artist => {
-
   var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
   var bandName = artist.split('+').join(" ");
 
   axios.get(queryUrl).then(
@@ -34,88 +30,59 @@ const concertSearch = artist => {
 }
 
 const songSearch = song => {
-  var queryUrl = "";
+  var songName = song.split('+').join(" ");
 
-  var songName = movie.split('+').join(" ");
+  spotify.search({
+    type: 'track',
+    query: songName
+  }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log(data.tracks.items[0]);
+    songTitle = data.tracks.items[0].name;
+    songURL = data.tracks.items[0].preview_url;
+    songAlbum = data.tracks.items[0].album.name;
 
-  axios.get(queryUrl).then(
-    function(response) {
-      //  * Artist(s)
-      //
-      //   * The song's name
-      //
-      //   * A preview link of the song from Spotify
-      //
-      //   * The album that the song is from
-      //
-      // * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-    });
-
+    console.log(`\nSong Title: "${songTitle}"\n Check it out here: ${songURL}\n Album: "${songAlbum}"`);
+  });
   fs.appendFile('log.txt', (',' + command + ',' + songName), (err) => {
     if (err) throw err;
   });
 }
 
 const movieSearch = movie => {
-  if (movie.length > 1) {
+  var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  var movieName = movie.split('+').join(" ");
 
-    var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  axios.get(queryUrl).then(
+    function(response) {
+      //* Title of the movie.
+      var title = response.data.Title;
+      // * Year the movie came out.
+      var year = response.data.Year;
+      // * IMDB Rating of the movie.
+      var rating = response.data.Ratings[0].Value;
+      // * Rotten Tomatoes Rating of the movie.
+      var rotTom = response.data.Ratings[1].Value;
+      // * Country where the movie was produced.
+      var country = response.data.Country;
+      // * Language of the movie.
+      var lanuage = response.data.Language;
+      // * Plot of the movie.
+      var plot = response.data.Plot;
+      // * Actors in the movie.
+      var actors = response.data.Actors;
 
-    var movieName = movie.split('+').join(" ");
-
-    axios.get(queryUrl).then(
-      function(response) {
-
-        //* Title of the movie.
-        var title = response.data.Title;
-        // * Year the movie came out.
-        var year = response.data.Year;
-        // * IMDB Rating of the movie.
-        var rating = response.data.Ratings[0].Value;
-        // * Rotten Tomatoes Rating of the movie.
-        var rotTom = response.data.Ratings[1].Value;
-        // * Country where the movie was produced.
-        var country = response.data.Country;
-        // * Language of the movie.
-        var lanuage = response.data.Language;
-        // * Plot of the movie.
-        var plot = response.data.Plot;
-        // * Actors in the movie.
-        var actors = response.data.Actors;
-
-        console.log(`"${title}" released in: ${response.data.Year}.
+      console.log(`"${title}" released in: ${response.data.Year}.
         It's IMDB rating is ${rating}, the Rotten Tomatoes rating is ${rotTom}.
         It was produced in ${country} and is in ${lanuage}.`);
-        console.log(`Plot Summary: "${plot}"
+      console.log(`Plot Summary: "${plot}"
         Starring: ${actors}`);
-      });
-    fs.appendFile('log.txt', (',' + command + ',' + movieName), (err) => {
-      if (err) throw err;
     });
-  } else {
-    // * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    var queryUrl = "https://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
-
-    axios.get(queryUrl).then(
-      function(response) {
-
-        var title = response.data.Title;
-        var year = response.data.Year;
-        var rating = response.data.Ratings[0].Value;
-        var rotTom = response.data.Ratings[1].Value;
-        var country = response.data.Country;
-        var lanuage = response.data.Language;
-        var plot = response.data.Plot;
-        var actors = response.data.Actors;
-
-        console.log(`"${title}" released in: ${response.data.Year}.
-        It's IMDB rating is ${rating}, the Rotten Tomatoes rating is ${rotTom}.
-        It was produced in ${country} and is in ${lanuage}.`);
-        console.log(`Plot Summary: "${plot}"
-        Starring: ${actors}`);
-      });
-  }
+  fs.appendFile('log.txt', (',' + command + ',' + movieName), (err) => {
+    if (err) throw err;
+  });
 }
 
 const readRand = () => {
@@ -125,8 +92,8 @@ const readRand = () => {
     }
     var dataArray = data.split(",");
 
-    // command = dataArray[0];
-    // songSearch(dataArray[1]);
+    command = dataArray[0];
+    songSearch(dataArray[1]);
 
     command = dataArray[2];
     movieSearch(dataArray[3]);
@@ -139,33 +106,45 @@ const readRand = () => {
 
 switch (command) {
   case 'concert-this':
-    var artistSearch = []
+    var artistSearch = [];
     for (var i = 3; i < process.argv.length; i++) {
       var userInput = process.argv[i];
       artistSearch.push(userInput);
     }
     var artist = artistSearch.join('+');
-    concertSearch(artist);
+    if (artist.length > 1) {
+      concertSearch(artist)
+    } else {
+      concertSearch('Ariana Grande')
+    }
     break;
 
   case 'spotify-this-song':
-    var musicSearch = []
+    var musicSearch = [];
     for (var i = 3; i < process.argv.length; i++) {
       var userInput = process.argv[i];
       musicSearch.push(userInput);
     }
     var song = musicSearch.join('+');
-    songSearch(song);
+    if (song.length > 1) {
+      songSearch(song)
+    } else {
+      songSearch('The Sign')
+    }
     break;
 
   case 'movie-this':
     var fullTitle = [];
     for (var i = 3; i < process.argv.length; i++) {
-      var titleSearch = process.argv[i];
-      fullTitle.push(titleSearch);
+      var userInput = process.argv[i];
+      fullTitle.push(userInput);
     }
     var movie = fullTitle.join('+');
-    movieSearch(movie);
+    if (movie.length > 1) {
+      movieSearch(movie)
+    } else {
+      movieSearch('Mr. Nobody')
+    }
     break;
 
   case 'do-what-it-says':
